d562463a24510b836d3ca66c18b98e0b79b862de
Kamaludin <160387206+Kamali6626@users.noreply.github.com>
2026-06-14 23:21:54 +0500

    Add The 67th 6-7 Integer Problem [B] from Codeforces

---
+++ 2218/B - The 67th 6-7 Integer Problem/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while(t--) {
+        int x;
+        int sum = 0;
+        int mx = -100;
+ 
+        for(int i = 0; i < 7; i++) {
+            cin >> x;
+            sum += x;
+            mx = max(mx, x);
+        }
+ 
+        cout<<2 * mx - sum<<endl;
+    }
+ 
+    return 0;
+}
