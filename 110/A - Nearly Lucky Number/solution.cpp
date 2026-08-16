dbdecebc08979eedf238e56f1802d61e6325e914
Kamaludin <160387206+Kamali6626@users.noreply.github.com>
2026-08-16 23:33:54 +0500

    Add Nearly Lucky Number [A] from Codeforces

---
+++ 110/A - Nearly Lucky Number/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+long long n;
+cin>>n;
+int count=0;
+while(n>0){
+    int rem=0;
+    
+    rem=n%10;
+    if(rem==4||rem==7){
+       count++;
+    }
+       
+    n/=10;
+}
+if(count==4||count==7){
+    cout<<"YES";
+}else{
+    cout<<"NO";
+}
+    return 0;
+}
