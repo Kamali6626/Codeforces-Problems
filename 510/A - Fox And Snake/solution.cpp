bdc7a888026853d716da5fce06e946c9ed40e514
Kamaludin <160387206+Kamali6626@users.noreply.github.com>
2026-08-16 23:34:55 +0500

    Add Fox And Snake [A] from Codeforces

---
+++ 510/A - Fox And Snake/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    int n, m;
+    cin>>n>>m;
+ 
+    bool right = true;
+ 
+    for(int i = 1; i <= n; i++) {
+        if(i % 2 == 1) {
+            for(int j = 0; j < m; j++)
+                cout << "#";
+        } else{
+            if(right) {
+                for(int j = 0; j < m - 1; j++)
+                    cout << ".";
+                cout << "#";
+            }else {
+                cout << "#";
+                for(int j = 0; j < m - 1; j++)
+                    cout << ".";
+            }
+            right = !right;
+        }
+        cout<<endl;
+    }
+ 
+    return 0;
+}
