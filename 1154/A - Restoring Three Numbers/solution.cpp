814edd63fb69ebe3b4dfa97bd52bd7e199f854ea
Kamaludin <160387206+Kamali6626@users.noreply.github.com>
2026-08-22 22:23:55 +0500

    Add Restoring Three Numbers [A] from Codeforces

---
+++ 1154/A - Restoring Three Numbers/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ 
+int main() {
+    vector<int>x(4);
+    cin>>x[0]>>x[1]>>x[2]>>x[3];
+    sort(x.begin(), x.end());
+    int a = x[3] - x[2];
+    int b = x[3] - x[1];
+    int c = x[3] - x[0];
+    cout<<a<<" "<<b<<" "<<c<<endl;
+ 
+    return 0;
+}
